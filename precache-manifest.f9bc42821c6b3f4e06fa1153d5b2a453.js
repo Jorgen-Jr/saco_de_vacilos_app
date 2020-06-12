84fc3819f496dc6ba8ba54589d8cdcfba0645ea5
Jorge Antônio da Silva Nascimento Junior <jorgenjr@outlook.com>
2020-06-12 13:28:47 -0300

    Updates

---
+++ precache-manifest.f9bc42821c6b3f4e06fa1153d5b2a453.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1cbc7f0ed56562fc19d1a6e6b6eb69",
+    "url": "/saco_de_vacilos_frontend/index.html"
+  },
+  {
+    "revision": "a6fef737b93963a42de4",
+    "url": "/saco_de_vacilos_frontend/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "69f9bff59c00e628da23",
+    "url": "/saco_de_vacilos_frontend/static/css/main.f9952dc7.chunk.css"
+  },
+  {
+    "revision": "a6fef737b93963a42de4",
+    "url": "/saco_de_vacilos_frontend/static/js/2.3033a8bc.chunk.js"
+  },
+  {
+    "revision": "748c16e3efdd9e6a948a3d2d58878b88",
+    "url": "/saco_de_vacilos_frontend/static/js/2.3033a8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f9bff59c00e628da23",
+    "url": "/saco_de_vacilos_frontend/static/js/main.5a47c6db.chunk.js"
+  },
+  {
+    "revision": "d152fe247f29baa26a33",
+    "url": "/saco_de_vacilos_frontend/static/js/runtime-main.c1a63b47.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/saco_de_vacilos_frontend/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/saco_de_vacilos_frontend/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/saco_de_vacilos_frontend/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
